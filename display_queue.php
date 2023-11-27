5efad2246e4238ef360a2952f560372b518b4355
Yuwiiii <100648555+Yuwiiii@users.noreply.github.com>
2023-11-27 20:36:22 +0800

    New Academics Display
    Added the following:
    *New interface
    *Display only the queue number that is being called
    ______
    
    Ongoing notification sound

--- display_queue.php
+++ display_queue.php
@@ -11,61 +11,80 @@ $getprogramDataSqlresult = $conn->query($getprogramDataSql);
 
 $displayedProgram = [];
 
-if ($getprogramDataSqlresult->num_rows > 0) {
-    while ($row = $getprogramDataSqlresult->fetch_assoc()) {
-        $acronym = $row["acronym"];
+        if ($getprogramDataSqlresult->num_rows > 0) {
+            while ($row = $getprogramDataSqlresult->fetch_assoc()) {
+                $acronym = $row["acronym"];
 
-        if (!in_array($acronym, $displayedProgram)) {
-            echo '<div class="' . $acronym . ' office">';
-            echo "<div class='header-title'>";
-            echo "<p>" . $acronym . "</p>";
-            echo "</div>";
-
-            $getcourseDataSql = "SELECT pc.program, pc.course
-                FROM program_chairs AS pc
-                JOIN colleges AS c ON pc.program = c.acronym
-                WHERE c.acronym = '$acronym'";
-            $getcourseDataSqlresult = $conn->query($getcourseDataSql);
-            $displayedCourses = [];
-            $courseCount = 0;
-
-            while ($row = $getcourseDataSqlresult->fetch_assoc()) {
-                $course = $row["course"];
-
-                if (!in_array($course, $displayedCourses)) {
-                    $courseCount++;
-                    echo "<div class='list-div";
-                    
-                    // Check if it's the last list-div
-                    if ($courseCount === $getcourseDataSqlresult->num_rows) {
-                        echo " last-list-div";
-                    }
-
-                    echo "'>";
-                    echo "<p><b>" . $course . ":</b></p>";
-
-                    $getqueuenumerDataSql = "SELECT aq.queue_number, pc.full_name, pc.course
-                        FROM academics_queue AS aq 
-                        JOIN program_chairs AS pc ON aq.concern = pc.full_name 
-                        WHERE aq.status = 1 AND pc.course = '$course'";
-                    $getqueuenumerDataSqlresult = $conn->query($getqueuenumerDataSql);
-
-                    if ($getqueuenumerDataSqlresult->num_rows > 0) {
-                        while ($row = $getqueuenumerDataSqlresult->fetch_assoc()) {
-                            echo "<div class='qn-div'>";
-                            echo "<p><b>" . $row['queue_number'] . "</b></p>";
-                            echo "</div>";
-                        }
-                    } else {
-                        echo "<p>Loading...</p>";
-                    }
-
+                if (!in_array($acronym, $displayedProgram)) {
+                    echo '<div class="' . $acronym . ' office">';
+                    echo "<div class='header-title'>";
+                    echo "<p>" . $acronym . "</p>";
                     echo "</div>";
 
-                    // Add the course to the array to mark it as displayed
-                    $displayedCourses[] = $course;
+                    // Start here
+
+                    $getcourseDataSql = "SELECT pc.program, pc.course
+                        FROM program_chairs AS pc
+                        JOIN colleges AS c ON pc.program = c.acronym
+                        WHERE c.acronym = '$acronym'";
+                    $getcourseDataSqlresult = $conn->query($getcourseDataSql);
+                    $displayedCourses = [];
+                    $courseCount = 0;
+                    $hasQueueNumbers = false; // Flag variable
+
+                    // ...
+
+        while ($row = $getcourseDataSqlresult->fetch_assoc()) {
+            $course = $row["course"];
+            $getqueuenumerDataSql = "SELECT aq.queue_number
+                FROM academics_queue AS aq 
+                JOIN program_chairs AS pc ON aq.concern = pc.full_name 
+                WHERE aq.status = 1 AND pc.course = '$course'";
+            $getqueuenumerDataSqlresult = $conn->query($getqueuenumerDataSql);
+
+            // Check if there are queue numbers with status 1 for this course
+            if ($getqueuenumerDataSqlresult->num_rows > 0) {
+                $courseCount++;
+                echo "<div class='list-div";
+
+                // Check if it's the last list-div
+                if ($courseCount === $getcourseDataSqlresult->num_rows) {
+                    echo " last-list-div";
                 }
+
+                echo "'>";
+                echo "<p><b>" . $course . ":</b></p>";
+
+                // Trigger audio playback when a new list-div is generated
+                echo '<audio class="notificationSound" src="sound/queue_notification.mp3"></audio>';
+                // echo "<script>document.querySelector('.notificationSound').play();</script>";
+
+                // Display queue numbers with status 1 for this course
+                while ($row = $getqueuenumerDataSqlresult->fetch_assoc()) {
+                    echo "<div class='qn-div'>";
+                    echo "<p><b>" . $row['queue_number'] . "</b></p>";
+                    echo "</div>";
+                }
+
+                echo "</div>";
+
+                // Set the flag to true as there are queue numbers
+                $hasQueueNumbers = true;
+
+                // Add the course to the array to mark it as displayed
+                $displayedCourses[] = $course;
             }
+        }
+
+
+            // Check the flag and display "else" condition if no queue numbers
+            if (!$hasQueueNumbers) {
+                echo "<div class='que-div'>";
+                echo "<p>-</p>";
+                echo "</div>";
+            }
+
+            // End here
 
             echo "</div>";
 
