242d8b74eb0174ee6fb3dd34c5a79149be25673b
Nico <nicorobles122882@gmail.com>
2023-12-15 15:44:02 +0800

    update

--- check_student.php
+++ check_student.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $studentId = $_POST['studentId'];
 
         // Check if the student ID exists in the queue table and has status 1
-        $sql = "SELECT * FROM queue WHERE student_id = '$studentId' AND status = 0";
+        $sql = "SELECT * FROM queue WHERE student_id = '$studentId' AND studentstatus = 0";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
--- script/client.js
+++ script/client.js
@@ -19,23 +19,27 @@
 
 //   window.location.href = "nulqueue.php";
 // }
+function closeAlert() {
+    $('#ongoingQueueAlert').fadeOut();
+}
+
 
 function submitStudentId() {
-  var studentId = document.getElementById("studentId").value;
-  var program = document.getElementById("program").value;
+    var studentId = document.getElementById("studentId").value;
+    var program = document.getElementById("program").value;
 
-  if (studentId === "") {
-      document.getElementById("error-message").style.display = "block";
-      return;
-  }
+    if (studentId === "") {
+        document.getElementById("error-message").style.display = "block";
+        return;
+    }
 
-  // Set the student ID in local storage
-  localStorage.setItem("studentId", studentId);
-  localStorage.setItem("program", program);
+    // Set the student ID in local storage
+    localStorage.setItem("studentId", studentId);
+    localStorage.setItem("program", program);
 
-  if (studentId === "") {
-      return;
-  }
+    if (studentId === "") {
+        return;
+    }
 
     // Make an AJAX request to check the queue status
     $.ajax({
@@ -47,13 +51,16 @@ function submitStudentId() {
             if (response.status === 'error') {
                 // Display a pop-up or show a message indicating an ongoing queue
                 $('#ongoingQueueAlert').fadeIn();
+
+                // Close the alert after 5 seconds (5000 milliseconds)
+                setTimeout(closeAlert, 5000);
             } else {
                 // Set the student ID in local storage
                 localStorage.setItem("studentId", studentId);
                 localStorage.setItem("program", program);
 
                 // Redirect to the queue page
-                
+
             }
         },
         error: function () {
@@ -64,192 +71,192 @@ function submitStudentId() {
 
 // queue student
 function registerStudent() {
-  var studentId = localStorage.getItem("studentId");
-  var program = localStorage.getItem("program");
-  var office = document.getElementById("modalTitle1").innerText;
+    var studentId = localStorage.getItem("studentId");
+    var program = localStorage.getItem("program");
+    var office = document.getElementById("modalTitle1").innerText;
 
-  $.ajax({
-      type: "POST",
-      url: "process.php",
-      data: { studentId: studentId, program: program, office: office },
-      dataType: "json",
-      success: function (response) {
-          if (response.success) {
-              var queueNumber = response.queue_number;
-              document.getElementById("queueNumber").innerText = queueNumber;
-              $('#thirdModal').modal('show');
+    $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: { studentId: studentId, program: program, office: office },
+        dataType: "json",
+        success: function (response) {
+            if (response.success) {
+                var queueNumber = response.queue_number;
+                document.getElementById("queueNumber").innerText = queueNumber;
+                $('#thirdModal').modal('show');
 
-            // Notify target offices of new Queue
+                // Notify target offices of new Queue
 
-          } else {
-              alert("Error: " + response.message);
-          }
-      },
-      error: function () {
-          alert("An error occurred.");
-      }
-  });
+            } else {
+                alert("Error: " + response.message);
+            }
+        },
+        error: function () {
+            alert("An error occurred.");
+        }
+    });
 }
 
 
 // queue student
 function registerGuest() {
-  localStorage.setItem("studentId", "GUEST");
-  localStorage.setItem("program", "-");
-  var studentId = localStorage.getItem("studentId");
-  var program = localStorage.getItem("program");
-  var office = document.getElementById("modalTitle1").innerText;
+    localStorage.setItem("studentId", "GUEST");
+    localStorage.setItem("program", "-");
+    var studentId = localStorage.getItem("studentId");
+    var program = localStorage.getItem("program");
+    var office = document.getElementById("modalTitle1").innerText;
 
-  $.ajax({
-      type: "POST",
-      url: "process.php",
-      data: {
-          studentId: studentId,
-          program: program,
-          office: office
-      },
-      dataType: "json",
-      success: function (response) {
-          if (response.success) {
-              var queueNumber = response.queue_number;
-              // Set the queue number in the modal
-              document.getElementById("queueNumber").innerText = queueNumber;
-              // Show the third modal
-              $('#thirdModal').modal('show');
+    $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: {
+            studentId: studentId,
+            program: program,
+            office: office
+        },
+        dataType: "json",
+        success: function (response) {
+            if (response.success) {
+                var queueNumber = response.queue_number;
+                // Set the queue number in the modal
+                document.getElementById("queueNumber").innerText = queueNumber;
+                // Show the third modal
+                $('#thirdModal').modal('show');
 
-            // Notify target offices of new Queue
+                // Notify target offices of new Queue
 
-            console.log("transmitting message")
-          } else {
-              alert("Error: " + response.message);
-          }
-      },
-      error: function () {
-          alert("An error occurred.");
-      }
-  });
+                console.log("transmitting message")
+            } else {
+                alert("Error: " + response.message);
+            }
+        },
+        error: function () {
+            alert("An error occurred.");
+        }
+    });
 }
 
 // Function to update the modal titles
 function updateModalTitle(modalId, title) {
-  $(modalId).find(".modal-title").text(title);
+    $(modalId).find(".modal-title").text(title);
 }
 
 
 $(".btn").click(function () {
-  var modalTitle = $(this).data("title");
+    var modalTitle = $(this).data("title");
 
 
-  // Update modals
-  updateModalTitle("#firstModal", modalTitle);
-  updateModalTitle("#secondModal", modalTitle);
-  updateModalTitle("#thirdModal", modalTitle);
-  updateModalTitle("#acadModal", modalTitle);
-  updateModalTitle("#acadModal2", modalTitle);
-  updateModalTitle("#acadModal3", modalTitle);
-  // populateProgramChairs(modalTitle);
+    // Update modals
+    updateModalTitle("#firstModal", modalTitle);
+    updateModalTitle("#secondModal", modalTitle);
+    updateModalTitle("#thirdModal", modalTitle);
+    updateModalTitle("#acadModal", modalTitle);
+    updateModalTitle("#acadModal2", modalTitle);
+    updateModalTitle("#acadModal3", modalTitle);
+    // populateProgramChairs(modalTitle);
 });
 
 // Handle the submit button click event
 function insertAcads() {
-  var studentId = localStorage.getItem("studentId");
-  var selectedChair = $("#program-chair-select option:selected");
+    var studentId = localStorage.getItem("studentId");
+    var selectedChair = $("#program-chair-select option:selected");
 
-  var name = selectedChair.text().split('<---')[0].trim(); //cut the 
-  var program = document.getElementById("modalTitle1").innerText;
-  var program_queue = localStorage.getItem("program");
-  var office = document.getElementById("modalTitle1").innerText;
+    var name = selectedChair.text().split('<---')[0].trim(); //cut the 
+    var program = document.getElementById("modalTitle1").innerText;
+    var program_queue = localStorage.getItem("program");
+    var office = document.getElementById("modalTitle1").innerText;
 
 
-  // Send data to the server to insert into the 'academics' table
-  $.ajax({
-      url: "academics.php",
-      type: "POST",
-      data: {
-          concern: name,
-          program: program,
-          studentId: studentId,
-          office: office,
-          program_queue: program_queue,
-      },
-      dataType: "json",
-      success: function (response) {
-          if (response.success) {
-              var queueNumber = response.queue_number;
-              document.getElementById("queueNumber").innerText = queueNumber;
-              $('#acadModal3').modal('show');
-          } else {
-              alert("Error: " + response.message);
-          }
-      },
-      error: function () {
-          alert("An error occurred.");
-      }
-  });
+    // Send data to the server to insert into the 'academics' table
+    $.ajax({
+        url: "academics.php",
+        type: "POST",
+        data: {
+            concern: name,
+            program: program,
+            studentId: studentId,
+            office: office,
+            program_queue: program_queue,
+        },
+        dataType: "json",
+        success: function (response) {
+            if (response.success) {
+                var queueNumber = response.queue_number;
+                document.getElementById("queueNumber").innerText = queueNumber;
+                $('#acadModal3').modal('show');
+            } else {
+                alert("Error: " + response.message);
+            }
+        },
+        error: function () {
+            alert("An error occurred.");
+        }
+    });
 };
 
 function populateDropdown(programselected) {
-  // Create a function to update the dropdown options
-  function updateDropdown() {
-      $.ajax({
-          url: "academics.php",
-          type: "GET",
-          data: { program: programselected },
-          dataType: "json",
-          success: function (data) {
-              // Clear existing options
-              $('#program-chair-select').empty();
+    // Create a function to update the dropdown options
+    function updateDropdown() {
+        $.ajax({
+            url: "academics.php",
+            type: "GET",
+            data: { program: programselected },
+            dataType: "json",
+            success: function (data) {
+                // Clear existing options
+                $('#program-chair-select').empty();
 
-              // Add the retrieved options to the select element
-              $.each(data, function (key, value) {
-                  var option = $('<option>', {
-                      value: key,
-                      text: value.full_name
-                  });
+                // Add the retrieved options to the select element
+                $.each(data, function (key, value) {
+                    var option = $('<option>', {
+                        value: key,
+                        text: value.full_name
+                    });
 
-                  if (value.status === 'offline') {
-                      option.prop('disabled', true);
-                      option.text(value.full_name + ' <--- unavailable --->');
-                  }
-                  else if (value.status === 'unavailable') {
-                      option.prop('disabled', true);
-                      option.text(value.full_name + ' <--- unavailable --->');
-                  }
-                  else {
-                      option.text(value.full_name + ' <--- available --->');
-                  }
+                    if (value.status === 'offline') {
+                        option.prop('disabled', true);
+                        option.text(value.full_name + ' <--- unavailable --->');
+                    }
+                    else if (value.status === 'unavailable') {
+                        option.prop('disabled', true);
+                        option.text(value.full_name + ' <--- unavailable --->');
+                    }
+                    else {
+                        option.text(value.full_name + ' <--- available --->');
+                    }
 
-                  $('#program-chair-select').append(option);
-              });
-          },
-          error: function () {
-              console.error("Error fetching data from the server.");
-          }
-      });
-  }
+                    $('#program-chair-select').append(option);
+                });
+            },
+            error: function () {
+                console.error("Error fetching data from the server.");
+            }
+        });
+    }
 
-  // Initial call to populate the dropdown
-  updateDropdown();
+    // Initial call to populate the dropdown
+    updateDropdown();
 
 
 
-  $("#done-button").click(function () {
-      var selectedOption = $('#program-chair-select option:selected');
-      var selectedChair = selectedOption.text(); // Get the text of the selected option
+    $("#done-button").click(function () {
+        var selectedOption = $('#program-chair-select option:selected');
+        var selectedChair = selectedOption.text(); // Get the text of the selected option
 
-      var nameWithoutStatus = selectedChair.split('<---')[0].trim();
+        var nameWithoutStatus = selectedChair.split('<---')[0].trim();
 
-      $('#selected-chair').text(nameWithoutStatus);
-  });
+        $('#selected-chair').text(nameWithoutStatus);
+    });
 
 
 }
 
 function returnIndex() {
     window.location.href = 'index.php'
-  }
+}
 
-  $(document).ready(function () {
+$(document).ready(function () {
     $('#btn-print-qn').click(function () {
         $('#queueNumber, #modalTitle3, #desc').printThis({
             importCSS: true,
